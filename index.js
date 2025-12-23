a4d57d9983547c1242864568ef24851d190753c3
Muhammad-Zareef <muhammadzareef709@gmail.com>
2025-12-24 00:56:54 +0500

    initialize

---
+++ index.js
@@ -0,0 +1,41 @@
+
+const express = require('express');
+const app = express();
+const connectDB = require('./config/db');
+const fileUpload = require("express-fileupload");
+const adminRoutes = require('./routes/adminRoutes');
+const userRoutes = require('./routes/userRoutes');
+const resumeRoutes = require('./routes/resumeRoutes');
+const jobRoutes = require('./routes/jobRoutes');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+require("dotenv").config();
+
+app.use(fileUpload());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: "*",
+    credentials: true,
+}));
+
+(async function () {
+    try {
+        await connectDB();
+        console.log("DB connected successfully");
+    } catch (error) {
+        console.error("DB connection failed:", error);
+    }
+})();
+
+app.use('/api', userRoutes);
+app.use('/admin', adminRoutes);
+app.use('/api/resume', resumeRoutes);
+app.use('/api', jobRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+module.exports = app;
