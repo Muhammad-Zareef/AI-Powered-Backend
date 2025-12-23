eddcfa9235c125c04ecbb1e5e64e6e2cb5188734
Muhammad-Zareef <muhammadzareef709@gmail.com>
2025-12-24 01:09:10 +0500

    add api

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "*",
+    origin: "https://ai-powered-frontend-eight.vercel.app",
     credentials: true,
 }));
 
